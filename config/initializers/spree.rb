34e16166587a9145c01fba116c8469e0f54ab31c
alter-sachin <guitarplayersachin@gmail.com>
2016-10-21 00:49:30 -0400

    added dimensions for product image 350*250

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -20,7 +20,7 @@ Spree.user_class = "Spree::User"
 Spree::Image.class_eval do
   attachment_definitions[:attachment][:styles] = {
     :mini => '96x96>', # thumbs under image
-    :small => '300x200>', # images on category view
+    :small => '350x250>', # images on category view
     :product => '450x270>', # full product image
     :large => '1200x1200>' # light box image
   }
