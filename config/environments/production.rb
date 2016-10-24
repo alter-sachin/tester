cb094ea68e0c2a086a6bd83c57a0df665ef9b66e
alter-sachin <guitarplayersachin@gmail.com>
2016-10-24 03:02:34 -0400

    modified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  RAILS_DEFAULT_LOGGER = Logger.new('log/production.log')
 
   config.logger=Logger.new(STDOUT)
 end
